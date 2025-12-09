52023d38b2dc3683d78c5656567b04d525920def
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-12-09 12:35:30 +0530

    Add Main class with initial structure for House Robber solution

---
+++ House Robber/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main{
+    public static int(ArrayList<Integer> nums){
+
+    }
+    public static void main(String[] args){
+        Scanner kb=new Scanner(System.in);
+        if(kb.hasNextLine()){
+            String line =kb.nextLine();
+            Scanner LineScanner=new Scanner(line);
+            ArrayList<Integer> houses=new ArrayList<Integer>();
+            while(LineScanner.hasNextInt()){
+                
+            }
+        }
+    }
+}
